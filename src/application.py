a9b6d9025d12652d156e4ce74d9fec287e1fd392
Andrey <professor.cat@inbox>
2023-09-09 18:55:38 +0300

    Token is taking from settings now

--- src/application.py
+++ src/application.py
@@ -1,13 +1,12 @@
 from telegram.ext import Application, CommandHandler
 
 from bot.handlers import greeting
-
-TOKEN = 'TOKEN'
+from bot.core.settings import settings
 
 
 def main() -> None:
     """Start the bot"""
-    app = Application.builder().token(TOKEN).build()
+    app = Application.builder().token(settings.telegram_token).build()
     app.add_handler(CommandHandler('start', greeting))
     app.run_polling()
 
