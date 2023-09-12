e0d5d1fa27381f7c3d15c01fb0f4cedcb41aac9a
Андрей Киланов <8941519@mail.ru>
2023-09-12 20:54:45 +0500

    Add command help

--- src/application.py
+++ src/application.py
@@ -1,13 +1,16 @@
 from telegram.ext import Application, CommandHandler
 
-from bot.handlers import greeting
+from bot.conversations.command_application import help
 from bot.core.settings import settings
+from bot.handlers import greeting
 
 
 def main() -> None:
     """Start the bot"""
     app = Application.builder().token(settings.telegram_token).build()
     app.add_handler(CommandHandler('start', greeting))
+    app.add_handler(CommandHandler('help', help))
+
     app.run_polling()
 
 
