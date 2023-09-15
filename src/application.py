68aa0d18b6e5bcaabfe2a4e176475f080a7e6d78
Alex386386 <mamonov40600@mail.ru>
2023-09-15 13:26:36 +0300

    feature13/added functionality for checking access code 0.2

--- src/application.py
+++ src/application.py
@@ -1,16 +1,39 @@
-from telegram.ext import Application, CommandHandler
+from telegram.ext import (
+    Application,
+    CommandHandler,
+    ConversationHandler,
+    MessageHandler,
+    filters,
+)
 
-
-from bot.conversations.command_application import help, stop
+from bot.conversations.command_application import (
+    help,
+    stop,
+)
 from bot.core.settings import settings
-from bot.handlers import greeting
+from bot.handlers import (
+    greeting_callback,
+    done_callback,
+    check_the_secret_word_callback,
+)
+from bot.utils import CHECK
 
 
 def main() -> None:
     """Start the bot"""
     app = Application.builder().token(settings.telegram_token).build()
-    app.add_handler(CommandHandler('start', greeting))
-    app.add_handler(CommandHandler('help', help))
+    conv_handler = ConversationHandler(
+        entry_points=[CommandHandler("start", greeting_callback)],
+        states={
+            CHECK: [
+                MessageHandler(filters.TEXT, check_the_secret_word_callback),
+            ],
+        },
+        fallbacks=[MessageHandler(filters.Regex("^Done$"), done_callback)],
+    )
+
+    app.add_handler(conv_handler)
+    app.add_handler(CommandHandler("help", help))
     app.add_handler(CommandHandler("stop", stop))
 
     app.run_polling()
--- src/bot/handlers.py
+++ src/bot/handlers.py
@@ -1,9 +1,43 @@
 from telegram import Update
-from telegram.ext import ContextTypes
+from telegram.ext import ContextTypes, ConversationHandler
 
-from bot.constants.info.text import GREETING
+from bot.constants.text import (
+    START_MESSAGE,
+    FAILED_THE_TEST,
+    PASSED_THE_TEST,
+)
+from bot.core.settings import settings
+from bot.utils import (
+    STICKER_ID,
+    CHECK,
+)
 
 
-async def greeting(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+async def greeting_callback(
+    update: Update, context: ContextTypes.DEFAULT_TYPE
+) -> int:
     """Base handler to greet the user"""
-    await update.message.reply_text(GREETING)
+    await update.message.reply_text(START_MESSAGE)
+    return CHECK
+
+
+async def check_the_secret_word_callback(
+    update: Update, context: ContextTypes.DEFAULT_TYPE
+) -> int:
+    """Function that checks the secret word for access to the application."""
+    text = update.message.text
+    if text.lower() != settings.secret_word.lower():
+        await update.message.reply_text(FAILED_THE_TEST)
+        return CHECK
+    await update.message.reply_sticker(STICKER_ID)
+    await update.message.reply_text(PASSED_THE_TEST)
+    # Впоследствии вызов функции done заменить на возвращение значения следующего шага.
+    return await done_callback(update, context)
+
+
+async def done_callback(
+    update: Update, context: ContextTypes.DEFAULT_TYPE
+) -> int:
+    """End the conversation."""
+    await update.message.reply_text("Возвращайтесь, буду рад пообщаться!")
+    return ConversationHandler.END
