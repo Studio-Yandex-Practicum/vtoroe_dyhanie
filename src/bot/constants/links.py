25ad11bd1ea82a734dedc47a41c22a628b6a126e
Andrey <professor.cat@inbox>
2023-09-28 20:02:30 +0300

    feature22/bring actual links

--- src/bot/constants/links.py
+++ src/bot/constants/links.py
@@ -1,6 +1,6 @@
 PROCESSES_LINK = (
-    'https://miro.com/app/board/uXjVMpVIC0o=/?'
-    'moveToWidget=3458764563004871231&cot=14'
+    'https://drive.google.com/file/d/'
+    '11Oh9YIbvPuy4aZNNTRAzI0Oy0kJ7U0iG/view?usp=sharing'
 )
 
 ANNUAL_REPORTS_LINK = (
