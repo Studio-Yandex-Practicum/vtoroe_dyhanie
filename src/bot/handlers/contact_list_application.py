348c7743232b663b180a9b96aca3d3fe26db4db8
Dmitry Korepanov <dnker@mail.ru>
2023-10-12 23:28:43 +0500

    Modif faq_text,links,text,list_app under part """

--- src/bot/handlers/contact_list_application.py
+++ src/bot/handlers/contact_list_application.py
@@ -1,6 +1,9 @@
 from telegram import Update
 from telegram.ext import (
-    ContextTypes, ConversationHandler, filters, MessageHandler
+    ContextTypes,
+    ConversationHandler,
+    filters,
+    MessageHandler,
 )
 
 from bot.constants import contact_list_text
@@ -11,7 +14,7 @@ from bot.keyboards.keyboards import main_menu_markup
 
 
 async def contact_list_callback(
-        update: Update, context: ContextTypes.DEFAULT_TYPE
+    update: Update, context: ContextTypes.DEFAULT_TYPE
 ) -> int:
     """Обрабатывает кнопку "Список контактов" из главного меню."""
     await update.message.reply_text(
@@ -24,13 +27,14 @@ async def contact_list_callback(
 
 
 async def find_contact_by_fio(
-        update: Update,
-        context: ContextTypes.DEFAULT_TYPE,
+    update: Update,
+    context: ContextTypes.DEFAULT_TYPE,
 ) -> int:
     """Стартует поиск контакта по заданному тексту
     и возвращает контакт (телефон, имя...)"""
     # тут нужно будет написать что-то, что получит на вход текст
-    # query.data из query = update.callback_query и пойдет в базу искать контакты,
+    # query.data из query = update.callback_query и пойдет
+    # в базу искать контакты,
     # а на выходе вернет контакт
     # и пропишет результат в строчку ниже
 
@@ -44,8 +48,7 @@ async def find_contact_by_fio(
 contact_list_conv_handler = ConversationHandler(
     entry_points=[
         MessageHandler(
-            filters.Text([MENU_CONTACT_LIST]),
-            contact_list_callback
+            filters.Text([MENU_CONTACT_LIST]), contact_list_callback
         )
     ],
     states={
@@ -53,5 +56,5 @@ contact_list_conv_handler = ConversationHandler(
             MessageHandler(filters.TEXT, find_contact_by_fio),
         ]
     },
-    fallbacks=[MessageHandler(filters.Regex("^Done$"), done_callback)],
+    fallbacks=[MessageHandler(filters.Regex('^Done$'), done_callback)],
 )
