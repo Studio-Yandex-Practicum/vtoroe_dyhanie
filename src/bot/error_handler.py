cc13114e606b8541dbf92b4ccc41cf9dbdcb71b3
Настасья Мартынова <martynovanastasya@gmail.com>
2023-10-19 18:58:50 +0300

    feature error_handler fix

--- src/bot/error_handler.py
+++ src/bot/error_handler.py
@@ -5,9 +5,9 @@ from telegram.ext import ContextTypes
 
 
 async def error_handler(
-    update: object, context: ContextTypes.DEFAULT_TYPE
+    update: Update, context: ContextTypes.DEFAULT_TYPE
 ) -> None:
-    """Log the error while Update."""
+    """Логи ошибок при обработке обновлений."""
     logging.error(
         msg='Exception while handling an update:', exc_info=context.error
     )
