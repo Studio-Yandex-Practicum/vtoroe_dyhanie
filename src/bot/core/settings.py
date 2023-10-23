badb8aaf78198a02080d49bafdb1de8d74262f9c
Анастасия Савельева <Visteria09@yandex.ru>
2023-10-23 19:52:55 +0700

    изменения по ревью

--- src/bot/core/settings.py
+++ src/bot/core/settings.py
@@ -1,10 +1,19 @@
+from dotenv import load_dotenv
 from pydantic_settings import BaseSettings
+
+
+load_dotenv()
 
 
 class Settings(BaseSettings):
     telegram_token: str
     debug: bool = False
     secret_word: str = 'Бруня'
+    smtp_server: str = 'smtp.yandex.ru'
+    port: int = 465
+    sender_email: str
+    receiver_email: str
+    password_email: str
 
     class Config:
         env_file = '.env'
--- src/bot/utils.py
+++ src/bot/utils.py
@@ -1,36 +1,37 @@
 import smtplib
 import ssl
 
+from core.settings import settings
+
 
 def send_email(subject, body_text):
-    """
-    Send an email
-    """
-
-    smtp_server = "smtp.gmail.com"
-    port = 465   # для SSL подключения
-    sender_email = "esperansa.stitch@gmail.com"  # Емайл отправителя
-    receiver_email = "visteria09@yandex.ru"  # Емайл получателя
-    password = input("Введите пароль и нажмите Enter: ")
-
+    '''
+    Отправляет сообщение на электронную почту.
+    '''
     # Создание безопасного контекста SSL
     context = ssl.create_default_context()
 
     message = '\r\n'.join((
-        f'From: {sender_email}',
-        f'To: {receiver_email}',
+        f'From: {settings.sender_email}',
+        f'To: {settings.receiver_email}',
         f'Subject: {subject}',
         '',
         body_text))
 
-    with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
-        server.login(sender_email, password)
-        server.sendmail(sender_email, receiver_email, message.encode('UTF-8'))
+    with smtplib.SMTP_SSL(settings.smtp_server,
+                          settings.port,
+                          context=context) as server:
+        server.login(settings.sender_email, settings.password_email)
+        server.sendmail(settings.sender_email,
+                        settings.receiver_email,
+                        message.encode('UTF-8'))
 
 
 subject = 'Тестирование'
-body_text = '''Сотрудники Фонда «Второе дыхание» посетили праздник урожая в
-Кадыйском психоневрологическом интернате и провели выдачу вещевой помощи для
-проживающих и сотрудников'''
+body_text = '''Ненужная одежда — огромный ресурс и катализатор изменений. Она
+важна для того, чтобы бездомный не замёрз на улице, а девушка из нуждающейся
+семьи пошла на свой первый в жизни выпускной бал красивой. Одежда не только
+помогает людям, но и создает рабочие места и даже может вернуться в нашу жизнь
+в качестве набивки для дивана. '''
 
 send_email(subject, body_text)
