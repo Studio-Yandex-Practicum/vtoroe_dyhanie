b4d27b02355e626d3f8882d2738d71e2353ed961
Mikhail <green_panda@inbox.ru>
2023-11-04 22:04:40 +0300

    Add all models

--- src/admindjango_bot/admin_bot/admin.py
+++ src/admindjango_bot/admin_bot/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from admin_bot.models import AboutFundKeyboard, BasicInfoKeyboard, Keyboard
+
+
+class BaseModelAdmin(admin.ModelAdmin):
+    list_display = ('keyboard_name', 'keyboard_text')
+    empty_value_display = '-пусто-'
+    # readonly_fields = ('keyboard_name',)
+
+    class Meta:
+        abstract = True
+
+
+myModels = [AboutFundKeyboard, BasicInfoKeyboard, Keyboard]
+admin.site.register(myModels, BaseModelAdmin)
--- src/admindjango_bot/admin_bot/models.py
+++ src/admindjango_bot/admin_bot/models.py
@@ -1,9 +1,108 @@
 from django.db import models
 
 
-class AboutFundKeyboards(models.Model):
+class BasicModelKeyboard(models.Model):
+    keyboard_text = models.CharField(
+        max_length=100,
+        verbose_name='Текст внутри кнопки')
     keyboard_name = models.CharField(unique=False, max_length=100)
-    keyboard_text = models.CharField(max_length=100)
 
     class Meta:
-        db_table = "about_fund_keyboards"
+        abstract = True
+
+
+class AboutFundKeyboard(BasicModelKeyboard):
+    class Meta:
+        db_table = 'about_fund_keyboards'
+        verbose_name_plural = ('Модуль с реализацией '
+                               'клавиатур для блока "О Фонде"')
+
+
+class BasicInfoKeyboard(BasicModelKeyboard):
+    class Meta:
+        db_table = 'basic_info_keyboards'
+        verbose_name_plural = ('Базовые информационные кнопки')
+
+
+class Keyboard(BasicModelKeyboard):
+    class Meta:
+        db_table = 'keyboards'
+        verbose_name_plural = ('Гл.Меню и FAQ кнопки')
+
+
+class OnboardingKeyboard(BasicModelKeyboard):
+    class Meta:
+        db_table = 'onboarding_keyboards'
+
+
+class RulesKeyboard(BasicModelKeyboard):
+    class Meta:
+        db_table = 'rules_keyboards'
+
+
+class BasicModelConstant(models.Model):
+    constant_name = models.CharField(unique=False, max_length=100)
+    constant_text = models.TextField()
+
+    class Meta:
+        abstract = True
+
+
+class AboutFundText(BasicModelConstant):
+    class Meta:
+        db_table = 'about_fund_text'
+
+
+class BasicInfoText(BasicModelConstant):
+    class Meta:
+        db_table = 'basic_info_text'
+
+
+class Button(BasicModelConstant):
+    class Meta:
+        db_table = 'button'
+
+
+class ContactListtext(BasicModelConstant):
+    class Meta:
+        db_table = 'contact_list_text'
+
+
+class FaqText(BasicModelConstant):
+    class Meta:
+        db_table = 'faq_text'
+
+
+class Links(BasicModelConstant):
+    class Meta:
+        db_table = 'links'
+
+
+class OnboardingText(BasicModelConstant):
+    class Meta:
+        db_table = 'onboarding_text'
+
+
+class QueryPatterns(BasicModelConstant):
+    class Meta:
+        db_table = 'query_patterns'
+
+
+class RegFormsText(BasicModelConstant):
+    class Meta:
+        db_table = 'reg_forms_text'
+
+
+class RulesText(BasicModelConstant):
+    class Meta:
+        db_table = 'rules_text'
+
+
+class State(BasicModelConstant):
+    class Meta:
+        db_table = 'state'
+
+
+class Text(BasicModelConstant):
+    class Meta:
+        db_table = 'text'
