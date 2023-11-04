bbec4a9900943350d06b5691e2728e909b9d8bf4
unknown <heart.of.bass@gmail.com>
2023-11-04 22:49:51 +0300

    Admin models added

--- src/admindjango_bot/admin_bot/admin.py
+++ src/admindjango_bot/admin_bot/admin.py
@@ -1,9 +1,26 @@
+from admin_bot.models import (
+    AboutFundKeyboard,
+    AboutFundText,
+    BasicInfoKeyboard,
+    BasicInfoText,
+    Button,
+    ContactListtext,
+    FaqText,
+    Keyboard,
+    Links,
+    OnboardingKeyboard,
+    OnboardingText,
+    QueryPatterns,
+    RegFormsText,
+    RulesKeyboard,
+    RulesText,
+    State,
+    Text,
+)
 from django.contrib import admin
 
-from admin_bot.models import AboutFundKeyboard, BasicInfoKeyboard, Keyboard
 
-
-class BaseModelAdmin(admin.ModelAdmin):
+class BaseModelAdminKeys(admin.ModelAdmin):
     list_display = ('keyboard_name', 'keyboard_text')
     empty_value_display = '-пусто-'
     # readonly_fields = ('keyboard_name',)
@@ -12,5 +29,36 @@ class BaseModelAdmin(admin.ModelAdmin):
         abstract = True
 
 
-myModels = [AboutFundKeyboard, BasicInfoKeyboard, Keyboard]
-admin.site.register(myModels, BaseModelAdmin)
+class BaseModelAdminConstant(admin.ModelAdmin):
+    list_display = ('constant_name', 'constant_text')
+    empty_value_display = '-пусто-'
+    # readonly_fields = ('constant_name',)
+
+    class Meta:
+        abstract = True
+
+
+myModelsKeys = [
+    AboutFundKeyboard,
+    BasicInfoKeyboard,
+    Keyboard,
+    OnboardingKeyboard,
+    RulesKeyboard,
+]
+myModelsConstant = [
+    AboutFundText,
+    BasicInfoText,
+    Button,
+    ContactListtext,
+    FaqText,
+    Links,
+    OnboardingText,
+    QueryPatterns,
+    RegFormsText,
+    RulesText,
+    State,
+    Text,
+]
+
+admin.site.register(myModelsKeys, BaseModelAdminKeys)
+admin.site.register(myModelsConstant, BaseModelAdminConstant)
