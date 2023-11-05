7bc85716133990b0513fdffd6c90a2454bea75f1
unknown <heart.of.bass@gmail.com>
2023-11-05 22:10:35 +0300

    Migrations added. Verbose_name_plural added to the models.

--- src/admindjango_bot/admin_bot/models.py
+++ src/admindjango_bot/admin_bot/models.py
@@ -3,8 +3,8 @@ from django.db import models
 
 class BasicModelKeyboard(models.Model):
     keyboard_text = models.CharField(
-        max_length=100,
-        verbose_name='Текст внутри кнопки')
+        max_length=100, verbose_name='Текст внутри кнопки'
+    )
     keyboard_name = models.CharField(unique=False, max_length=100)
 
     class Meta:
@@ -14,20 +14,21 @@ class BasicModelKeyboard(models.Model):
 class AboutFundKeyboard(BasicModelKeyboard):
     class Meta:
         db_table = 'about_fund_keyboards'
-        verbose_name_plural = ('Модуль с реализацией '
-                               'клавиатур для блока "О Фонде"')
+        verbose_name_plural = (
+            'Модуль с реализацией ' 'клавиатур для блока "О Фонде"'
+        )
 
 
 class BasicInfoKeyboard(BasicModelKeyboard):
     class Meta:
         db_table = 'basic_info_keyboards'
-        verbose_name_plural = ('Базовые информационные кнопки')
+        verbose_name_plural = 'Базовые информационные кнопки'
 
 
 class Keyboard(BasicModelKeyboard):
     class Meta:
         db_table = 'keyboards'
-        verbose_name_plural = ('Гл.Меню и FAQ кнопки')
+        verbose_name_plural = 'Гл.Меню и FAQ кнопки'
 
 
 class OnboardingKeyboard(BasicModelKeyboard):
@@ -51,58 +52,70 @@ class BasicModelConstant(models.Model):
 class AboutFundText(BasicModelConstant):
     class Meta:
         db_table = 'about_fund_text'
+        verbose_name_plural = 'about_fund_text'
 
 
 class BasicInfoText(BasicModelConstant):
     class Meta:
         db_table = 'basic_info_text'
+        verbose_name_plural = 'basic_info_text'
 
 
 class Button(BasicModelConstant):
     class Meta:
         db_table = 'button'
+        verbose_name_plural = 'button'
 
 
 class ContactListtext(BasicModelConstant):
     class Meta:
         db_table = 'contact_list_text'
+        verbose_name_plural = 'contact_list_text'
 
 
 class FaqText(BasicModelConstant):
     class Meta:
         db_table = 'faq_text'
+        verbose_name_plural = 'faq_text'
 
 
 class Links(BasicModelConstant):
     class Meta:
         db_table = 'links'
+        verbose_name_plural = 'links'
 
 
 class OnboardingText(BasicModelConstant):
     class Meta:
         db_table = 'onboarding_text'
+        verbose_name_plural = 'onboarding_text'
 
 
 class QueryPatterns(BasicModelConstant):
     class Meta:
         db_table = 'query_patterns'
+        verbose_name_plural = 'query_patterns'
 
 
 class RegFormsText(BasicModelConstant):
     class Meta:
         db_table = 'reg_forms_text'
+        verbose_name_plural = 'reg_forms_text'
 
 
 class RulesText(BasicModelConstant):
     class Meta:
         db_table = 'rules_text'
+        verbose_name_plural = 'rules_text'
 
 
 class State(BasicModelConstant):
     class Meta:
         db_table = 'state'
+        verbose_name_plural = 'state'
 
 
 class Text(BasicModelConstant):
     class Meta:
         db_table = 'text'
+        verbose_name_plural = 'text'
