396a6fcecf3c40f8842ee1c471f36495c4886bfb
Mac 2 <mac2@MacBook-Air--Mac-2.local>
2023-11-13 21:42:47 +0300

    feature 29: отправка сообщений по email(fix flake8)

--- src/bot/constants/schemas.py
+++ src/bot/constants/schemas.py
@@ -7,7 +7,8 @@ class QuestionModel(BaseModel):
     question: str
 
     @validator('question')
-    def check_question_length(cls, value):
+    def check_question_length(cls, value):  # noqa
+        '''Проверка на количество символов'''
         if not (3 <= len(value) <= 150):
             raise ValueError(
                 'Длина сообщения должна быть от 3 до 150 символов'
@@ -15,7 +16,8 @@ class QuestionModel(BaseModel):
         return value
 
     @validator('question')
-    def check_question_content(cls, value):
+    def check_question_content(cls, value):  # noqa
+        '''Проверка на кирилицу'''
         if not re.match(r'^[а-яА-Я0-9\s\W]*$', value):
             raise ValueError('Сообщение должно содержать только кириллицу.')
         return value
